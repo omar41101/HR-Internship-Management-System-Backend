51a58d29c69f3d7c2c54b17411ae530b407a7f5b
Yasmine-Hmida <hmidayasmiine@gmail.com>
2026-03-29 21:15:36 +0100

    Fix that the supervisor can only see his team members timetables, besides his own timetable

--- controllers/timetableController.js
+++ controllers/timetableController.js
@@ -262,7 +262,7 @@ export const getTimetableByUser = async (req, res, next) => {
     }
 
     // Check if the id (if supervisor) is the user's supervisor id
-    if (user.supervisorId.toString() !== req.user._id.toString()) {
+    if (req.user.role === "Supervisor" && user.supervisor_id && user.supervisor_id !== req.user._id.toString()) {
       throw new AppError("Unauthorized!", 403);
     }
 
